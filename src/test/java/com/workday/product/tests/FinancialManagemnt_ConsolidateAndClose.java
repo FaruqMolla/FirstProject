3f9e0b02fa3ca7cde92d201f09d6fa565d30e59b
FaruqMolla <momolla2016@gmail.com>
2020-09-25 10:41:30 -0400

    first commit in wp branch

---
+++ src/test/java/com/workday/product/tests/FinancialManagemnt_ConsolidateAndClose.java
@@ -0,0 +1,51 @@
+package com.workday.product.tests;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FinancialManagemnt_ConsolidateAndClose {
+	
+	WebDriver driver;
+	@BeforeTest
+	public void setup() {
+		System.out.println("setting up the browser");
+		
+		WebDriverManager.chromedriver().setup();
+	    driver = new ChromeDriver();
+		driver.get("https://www.workday.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	
+	}
+@Test
+public void closeAndCosolidateTest() {
+	
+	WebElement product=driver.findElement(By.xpath("//ul[@class='cmp-navigation__group']//span[contains(text(),'Products')]"));
+	Actions act = new Actions(driver);
+	act.moveToElement(product).build().perform();
+	WebElement FM=driver.findElement(By.xpath("//span[contains(text(),'Accounting, payables, receivables, cash and asset')]"));
+	act.moveToElement(FM).click().build().perform();
+	WebElement finance= driver.findElement(By.xpath("(//a[contains(text(),'Financial Management')])[2]"));
+	act.moveToElement(finance).build().perform();
+	//WebElement close=driver.findElement(By.xpath("//a[contains(text(),'Consolidate and Close')]"));
+	act.moveToElement(driver.findElement(By.xpath("//a[contains(text(),'Consolidate and Close')]"))).click().build().perform();
+
+}
+
+@AfterTest
+public void destroy() {
+	driver.close();
+}
+}
